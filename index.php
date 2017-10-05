f9ab1024bb2ff01b0d8d777220fc8dda6ca44abb
gijsbondue <gijs.bondue@student.arteveldehs.be>
2017-10-05 10:11:10 +0200

    index
    les2

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <div id='resultaat'>  
+        <?php
+            $naam = "gijs";
+            print  "<h1>Welkom" . $naam . "</h1>";
+        ?>
+    </div>
+
+</body>
+</html>
+
